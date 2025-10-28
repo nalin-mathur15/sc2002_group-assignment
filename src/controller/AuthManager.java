306bab4da74eda80acf1e19c7c761d39266abd6e
heehawwinner <martin.tan100@gmail.com>
2025-10-28 12:37:49 +0800

    testing commit, with codes of auth and internship manager, sample UML diagrams.

--- src/controller/AuthManager.java
+++ src/controller/AuthManager.java
@@ -1,5 +1,63 @@
 package controller;
 
+import java.util.*;
+import model.User;
+
 public class AuthManager {
     // authenticate log in and password change
+
+    private Map<String, User> users;
+    private Set<String> loggedInUsers;
+
+    public AuthManager() {
+        users = new HashMap<>();
+        loggedInUsers = new HashSet<>();
+    }
+
+    // Add user (used during initialization)
+    public void addUser(User user) {
+        users.put(user.getUserId(), user);
+    }
+
+    // Retrieve a user
+    public User getUser(String userId) {
+        return users.get(userId);
+    }
+
+    // Login authentication
+    public boolean login(String userId, String password) {
+        User user = users.get(userId);
+        if (user != null && user.getPassword().equals(password)) {
+            loggedInUsers.add(userId);
+            System.out.println("Login successful. Welcome, " + user.getName() + "!");
+            return true;
+        }
+        System.out.println("Invalid user ID or password.");
+        return false;
+    }
+
+    // Logout
+    public void logout(User user) {
+        if (user != null) {
+            loggedInUsers.remove(user.getUserId());
+            System.out.println("You have been logged out.");
+        }
+    }
+
+    // Change password
+    public boolean changePassword(User user, String oldPassword, String newPassword) {
+        if (user.getPassword().equals(oldPassword)) {
+            user.setPassword(newPassword);
+            System.out.println("Password changed successfully!");
+            return true;
+        } else {
+            System.out.println("Incorrect old password.");
+            return false;
+        }
+    }
+
+    // Check if logged in
+    public boolean isLoggedIn(String userId) {
+        return loggedInUsers.contains(userId);
+    }
 }
