dac2e5b6138cea9f341632ddf0b68ba5515de025
nalin-mathur15 <mathurnalin15@googlemail.com>
2025-10-30 19:56:49 +0800

    completed Staff and CompanyRep

--- src/entity/CompanyRepresentative.java
+++ src/entity/CompanyRepresentative.java
@@ -1,5 +1,40 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
 public class CompanyRepresentative extends User {
-    
+    private String company;
+    private String department;
+    private String position;
+    private boolean staffApproved;
+    private final List<String> postedInternshipIDs;
+
+    public CompanyRepresentative(String userID, String name, String pwd, String companyname, String dep, String position) {
+        super(userID, name, pwd, "CompanyRepresentative");
+        this.company = companyname;
+        this.department = dep;
+        this.position = position;
+        this.postedInternshipIDs = new ArrayList<>();
+    }
+
+    //getters
+    public String getCompanyName() { return this.company; }
+    public String getDepartment() { return this.department; }
+    public String getPosition() { return this.position; }
+    public List<String> getPostedInternshipIDs() { return this.postedInternshipIDs; }
+    public boolean approvedRepresentative() { return this.staffApproved; }
+
+    //setters
+    public void setCompanyName(String company) { this.company = company; }
+    public void setDepartment(String dep) { this.department = dep; }
+    public void setPosition(String pos) { this.position = pos; }
+    public void approveRepresentative(boolean approve) { this.staffApproved = approve; }
+    public boolean addInternship(String internshipID) { 
+        if(!this.postedInternshipIDs.contains(internshipID)) { this.postedInternshipIDs.add(internshipID);return true; }
+        return false;
+    }
+    public boolean removeInternship(String internshipID) {
+        if(this.postedInternshipIDs.contains(internshipID)) { this.postedInternshipIDs.remove(internshipID);return true; }
+        return false;
+    }
 }
--- src/entity/Staff.java
+++ src/entity/Staff.java
@@ -1,5 +1,16 @@
 package entity;
 
 public class Staff extends User {
-    
+    private String department;
+    public Staff(String staffID, String name, String pwd, String dep) {
+        super(staffID, name, pwd, "Staff");
+        this.department = dep;
+    }
+
+    //getter
+    public String getDepartment() { return this.department; }
+
+    //setter
+    public void setDepartment(String dep) { this.department = dep; }
 }
+
